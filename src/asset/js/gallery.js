a7c8c69206799d1ef09288a4dc7979f8b49f2f64
youngkwon02 <youngkwon02@naver.com>
2021-08-27 02:43:45 +0900

    feat : remove static project

--- src/asset/js/gallery.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  let photoViewThumb = document.querySelectorAll('.photo-view-thumbnail');
-  let photoViewThumbWidth = photoViewThumb[0].offsetWidth;
-  for(let i=0; i<photoViewThumb.length; i++) {
-    photoViewThumb[i].style.height = photoViewThumbWidth + 'px';
-  }
-}
