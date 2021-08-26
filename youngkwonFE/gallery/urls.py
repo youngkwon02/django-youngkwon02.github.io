f5b12f6b28f8435541f272ce74be6c350ba00c42
youngkwon02 <youngkwon02@naver.com>
2021-08-27 01:25:15 +0900

    feat : gallery page index pass to views

--- youngkwonFE/gallery/urls.py
+++ youngkwonFE/gallery/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-  path('/<int:page>/', views.gallery_view, name="gallery"),
+  path('', views.gallery_first_page, name="gallery_first"),
+  path('<int:page>/', views.gallery_view, name="gallery"),
 ]
--- youngkwonFE/gallery/views.py
+++ youngkwonFE/gallery/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def gallery_view(request):
-  return render(request, 'gallery.html')
+def gallery_first_page(request):
+  return render(request, 'gallery.html', {'page': 1})
+
+
+def gallery_view(request, page):
+  return render(request, 'gallery.html', {'page': page})
