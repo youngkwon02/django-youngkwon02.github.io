ead243b166300d1a828696c5a623baa746c8452f
youngkwon02 <youngkwon02@naver.com>
2021-08-27 01:51:54 +0900

    feat : gallery basic rendering

--- youngkwonFE/project/static/js/gallery.js
+++ youngkwonFE/project/static/js/gallery.js
@@ -1,7 +1,36 @@
-window.onload = () => {
+const thumbnailSizeSet = () => {
   let photoViewThumb = document.querySelectorAll('.photo-view-thumbnail');
   let photoViewThumbWidth = photoViewThumb[0].offsetWidth;
   for(let i=0; i<photoViewThumb.length; i++) {
     photoViewThumb[i].style.height = photoViewThumbWidth + 'px';
   }
+}
+
+const imageRendering = (page) => {
+  // @FIXME: modify below variable if you want to modify the number of images for each page
+  let numberOfImageForEachPage = 8;
+  let startIndex = (page-1)*numberOfImageForEachPage;
+  let endIndex = (page*numberOfImageForEachPage) - 1;
+  let maxIndex = GALLERY_INDEX.length -1;
+
+  if(startIndex > maxIndex){
+    location.href = "/gallery/";
+  } else if(endIndex > maxIndex){
+    endIndex = maxIndex;
+  }
+
+  let innerHTML = "";
+  for(let i=startIndex; i<=endIndex; i++) {
+    innerHTML += '<div class="photo-view-thumbnail">';
+    innerHTML += '<img src="/static/image/gallery/' + GALLERY_INDEX[i] + '">';
+    innerHTML += '<div class="photo-view-title">' + titleParsing(GALLERY_INDEX[i]) + '</div>';
+    innerHTML += '</div>';
+  }
+  
+  document.querySelector('.photo-view-container').innerHTML = innerHTML;
+  thumbnailSizeSet();
+}
+
+const titleParsing = (name) => {
+  return name.split('.')[0];
 }
