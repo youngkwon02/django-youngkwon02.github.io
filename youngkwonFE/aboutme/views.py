46e7d4262087c0db81e2a765cf9fb8bfe472400a
youngkwon02 <youngkwon02@naver.com>
2021-12-31 16:41:01 +0900

    chore: create aboutme template

--- youngkwonFE/aboutme/views.py
+++ youngkwonFE/aboutme/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def aboutme_view(request):
+    return render(request, 'aboutme.html')
--- youngkwonFE/youngkwonFE/urls.py
+++ youngkwonFE/youngkwonFE/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('project.urls')),
     path('gallery/', include('gallery.urls')),
     path('sns/', include('sns.urls')),
+    path('youngkwon/', include('aboutme.urls')),
 ]
