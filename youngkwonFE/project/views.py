4c04391f69127813670130425d7d1de7b8d57cc7
youngkwon02 <youngkwon02@naver.com>
2022-06-15 14:57:55 +0900

    chore: create spark_view function

--- youngkwonFE/project/views.py
+++ youngkwonFE/project/views.py
@@ -2,24 +2,28 @@ from django.shortcuts import render
 
 
 def main_view(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html')
 
 
 def ironmain_view(request):
-  return render(request, 'ironman.html')
+    return render(request, 'ironman.html')
 
 
 def starfall_view(request):
-  return render(request, 'starfall.html')
+    return render(request, 'starfall.html')
 
 
 def slingshot_view(request):
-  return render(request, 'slingshot.html')
+    return render(request, 'slingshot.html')
 
 
 def acid_world_view(request):
-  return render(request, 'acid-world.html')
+    return render(request, 'acid-world.html')
 
 
 def sunset_view(request):
-  return render(request, 'sunset.html')
+    return render(request, 'sunset.html')
+
+
+def spark_view(request):
+    return render(request, 'spark.html')
