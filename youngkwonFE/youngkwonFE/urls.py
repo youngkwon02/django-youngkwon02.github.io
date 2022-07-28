4aefc63268bb4d6ea78c839dac7827546d248d1b
youngkwon02 <youngkwon02@naver.com>
2022-07-28 02:48:59 +0000

    [FEAT] mysite.ini for deploy

--- youngkwonFE/youngkwonFE/urls.py
+++ youngkwonFE/youngkwonFE/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
